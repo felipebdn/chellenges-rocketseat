fa54e45730f9e4de3b63f6625216a888816cd4a6
Felipe Barbosa do Nascimento <dfelipebdn@gmail.com>
2023-06-12 19:59:04 -0300

    configuration dev dependencies

--- formulario-multi-step/tailwind.config.js
+++ formulario-multi-step/tailwind.config.js
@@ -6,12 +6,19 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      transparent: 'transparent',
+
+      'purple-mid': '#633BBC',
+      'purple-light': '#8257E5',
+
+      white: '#FFFFFF',
+      'gray-100': '#E1E1E6',
+      'gray-200': '#8D8D99',
+      'gray-300': '#7C7C8A',
+      'gray-400': '#323238',
+
+      'sucess-color': '#1D8841',
     },
   },
   plugins: [],
