2db7e5e77c2641ccc11498e30e8e0d9e40cda03b
Felipe Barbosa do Nascimento <dfelipebdn@gmail.com>
2023-06-16 22:16:09 -0300

    aplicado useEffet envolta da condicional da rota success

--- formulario-multi-step/src/app/success/page.tsx
+++ formulario-multi-step/src/app/success/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import { FormContext } from '@/context/formContext'
 import { useRouter } from 'next/navigation'
 
@@ -7,16 +7,22 @@ export default function Success() {
   const router = useRouter()
   const { formState, step } = useContext(FormContext)
 
-  if (step !== 4) {
-    router.push('/contato')
-  }
+  useEffect(() => {
+    if (step !== 4) {
+      router.push('/contato')
+    }
+  }, [router, step])
 
   return (
     <div>
       <h5 className="mb-4 text-xl font-bold text-purple-light">Seus dados</h5>
-      <pre>{JSON.stringify(formState.contato, null, 2)}</pre>
-      <pre>{JSON.stringify(formState.empresa, null, 2)}</pre>
-      <pre>{JSON.stringify(formState.projeto, null, 2)}</pre>
+      {formState && (
+        <>
+          <pre>{JSON.stringify(formState.contato, null, 2)}</pre>
+          <pre>{JSON.stringify(formState.empresa, null, 2)}</pre>
+          <pre>{JSON.stringify(formState.projeto, null, 2)}</pre>
+        </>
+      )}
     </div>
   )
 }
